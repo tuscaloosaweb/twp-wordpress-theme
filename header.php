ab6d4eaafd67e1464500c278d0ef45b4045e1b86
Rachel Carden <bamadesigner@gmail.com>
2015-07-29 00:13:41 -0500

    Revert "Fixing some accessibility issues"
    This reverts commit ba1ba2d9cf4c080a4e12b684e254f07457d87807.

--- header.php
+++ header.php
@@ -23,7 +23,7 @@ $theme_dir = get_stylesheet_directory_uri();
 
 </head>
 <body <?php body_class(); ?>>
-    <a href="#twp-content" id="skip-to-content">Skip to Content</a>
+    <a href="#" id="skip-to-content">Skip to Content</a>
 
     <div id="twp-wrapper">
 
@@ -31,10 +31,7 @@ $theme_dir = get_stylesheet_directory_uri();
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-                        <a href="<?php echo get_bloginfo( 'url' ); ?>">
-                            <h1 class="screen-reader-text"><?php echo get_bloginfo( 'title' ); ?></h1>
-                            <img class="twp-logo" src="<?php echo "{$theme_dir}/images/logos/twp-logo.svg"; ?>" alt="" />
-                        </a>
+                        <a href="<?php echo get_bloginfo( 'url' ); ?>"><img class="twp-logo" src="<?php echo "{$theme_dir}/images/logos/twp-logo.svg"; ?>" /></a>
                     </div>
                 </div>
             </div>
@@ -44,17 +41,13 @@ $theme_dir = get_stylesheet_directory_uri();
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-
-                        <div id="twp-content">
-                            <p>Tuscaloosa Web Professionals is a community and meetup group for anyone in the Tuscaloosa (and surrounding) area who makes a living building the world wide web. Whether you're a designer, coder, writer, sysadmin, or other web-related specialist, we invite you to join us to network, collaborate, learn, and share your expertise.</p>
-                            <p><strong>For more information, <a href="http://www.meetup.com/Tuscaloosa-Web-Professionals/">visit Tuscaloosa Web Professionals on meetup.com</a>.</strong></p>
-                            <div class="twp-social-media-buttons">
-                                <a href="https://twitter.com/tuscaloosaweb" class="btn btn-primary btn-twitter" title="Follow Tuscaloosa Web Professionals on Twitter"><span>Twitter</span></a>
-                                <a href="https://github.com/tuscaloosaweb" class="btn btn-primary btn-github" title="Follow Tuscaloosa Web Professionals on GitHub"><span>GitHub</span></a>
-                                <a href="http://www.meetup.com/Tuscaloosa-Web-Professionals/" class="btn btn-primary btn-meetup" title="Join Tuscaloosa Web Professionals on meetup.com"><span>Join us on meetup.com</span></a>
-                            </div>
+                        <p>Tuscaloosa Web Professionals is a community and meetup group for anyone in the Tuscaloosa (and surrounding) area who makes a living building the world wide web. Whether you're a designer, coder, writer, sysadmin, or other web-related specialist, we invite you to join us to network, collaborate, learn, and share your expertise.</p>
+                        <p><strong>For more information, <a href="http://www.meetup.com/Tuscaloosa-Web-Professionals/">visit Tuscaloosa Web Professionals on meetup.com</a>.</strong></p>
+                        <div class="twp-social-media-buttons">
+                            <a href="https://twitter.com/tuscaloosaweb" class="btn btn-primary btn-twitter" title="Follow Tuscaloosa Web Professionals on Twitter"><span>Twitter</span></a>
+                            <a href="https://github.com/tuscaloosaweb" class="btn btn-primary btn-github" title="Follow Tuscaloosa Web Professionals on GitHub"><span>GitHub</span></a>
+                            <a href="http://www.meetup.com/Tuscaloosa-Web-Professionals/" class="btn btn-primary btn-meetup" title="Join Tuscaloosa Web Professionals on meetup.com"><span>Join us on meetup.com</span></a>
                         </div>
-
                     </div>
                 </div>
             </div>
