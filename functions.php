26327ad7425c3cf8981356332e212567335591e2
Rachel Carden <bamadesigner@gmail.com>
2015-07-29 00:07:15 -0500

    Adding favicons

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
 
+//! Add favicons
+add_action( 'wp_head', 'tuscaloosa_web_add_favicons' );
+add_action( 'admin_head', 'tuscaloosa_web_add_favicons' );
+add_action( 'login_head', 'tuscaloosa_web_add_favicons' );
+function tuscaloosa_web_add_favicons() {
+
+    // Set the favicons folder
+    $favicons_folder = get_stylesheet_directory_uri() . '/images/favicons/';
+
+    // Include the favicons
+    ?><link rel="shortcut icon" href="<?php echo $favicons_folder; ?>twp-favicon-60x60.png" />
+    <link rel="apple-touch-icon" href="<?php echo $favicons_folder; ?>twp-favicon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $favicons_folder; ?>twp-favicon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $favicons_folder; ?>twp-favicon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $favicons_folder; ?>twp-favicon-152x152.png" /><?php
+
+}
+
 // Enqueue styles and scripts
 add_action( 'wp_enqueue_scripts', function() {
 
@@ -10,7 +28,7 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_register_style( 'twp-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,300' );
 
     // Enqueue our base stylesheet
-    wp_enqueue_style( 'twp', $theme_dir . '/css/style.min.css', array( 'twp-fonts' ), NULL );
+    wp_enqueue_style( 'tuscaloosa-web', $theme_dir . '/css/style.min.css', array( 'twp-fonts' ), NULL );
 
     // Modernizr JS - goes in header
     wp_enqueue_script( 'modernizr', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js' );
